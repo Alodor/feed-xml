9ccc2feef0d2c8d48a9b115ff8ea19492fa92771
Alodor <jose-a-mendez@hotmail.com>
2018-09-11 23:25:30 -0400

    Diseño de la vista aplicado con sass

--- index.php
+++ index.php
@@ -4,44 +4,52 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+        <link rel="stylesheet" href="css/style.css">
         <title>Feed Blog Post desde XML</title>
     </head>
     
     <body>
-        <?php 
-        // Captura de items
-        $items = $xml->channel->item;
-
-        // Recorrer los item del archivo
-        $count = 0;
-
-        foreach ($items as $item) {  
+        <section class="container">
+            <h1 class="title">Feed Blog Post desde XML</h1>
             
-            // Detener el recorrido en el cuarto item
-            if ($count === 4) break;
-    
-            $description = $item->description;
-            $image = extraerSRC($description);
-            $paragraph = explode('<p>', $description);
+            <div class="card-container">
+                <?php 
+                // Captura de items
+                $items = $xml->channel->item;
 
-            // Mostrar Items
-            $data = "";
-            $data .= "
-            <div class='card'>
-                <div class='card-header'>
-                    <a href='".$item->link."'>
-                        <img src='".$image."'>
-                    </a>
-                </div>
-                <div class='card-body'>
-                    <h3 class='card-title'>".$item->title."</h3>
-                    <p>".$paragraph[1]."</p>
-                </div>
+                // Recorrer los item del archivo
+                $count = 0;
+
+                foreach ($items as $item) {  
+
+                    // Detener el recorrido en el cuarto item
+                    if ($count === 4) break;
+
+                    $description = $item->description;
+                    $image = extraerSRC($description);
+                    $paragraph = explode('<p>', $description);
+
+                    // Mostrar Items
+                    $data = "";
+                    $data .= "
+                    <div class='card'>
+                        <div class='card-header'>
+                            <a href='".$item->link."'>
+                                <img class='thumbnail' src='".$image."'>
+                            </a>
+                        </div>
+                        <div class='card-body'>
+                            <h3 class='card-title'>".$item->title."</h3>
+                            <p class='card-text'>".$paragraph[1]."</p>
+                            <p><a class='read-more' href='".$item->link."'>Leer Más</a></p>
+                        </div>
+                    </div>
+                    ";
+
+                    echo $data;
+                    $count++;
+                } ?>        
             </div>
-            ";
-
-            echo $data;
-            $count++;
-        } ?>        
+        </section>
     </body>
 </html>
