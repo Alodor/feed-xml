09f7c781165c03ec22811be903f82651563f005b
Alodor <jose-a-mendez@hotmail.com>
2018-09-11 21:47:12 -0400

    Función para obtener la imagen de la descripción

--- feed.php
+++ feed.php
@@ -5,51 +5,8 @@ $xml = simplexml_load_file('https://inbound.digifianz.com/academy/rss.xml');
 
 
 // Funcion para extraer src de imagen
-function extraerSRC($string) {
-    preg_match('@src="([^"]+)"@', $string, $array);
+function extraerSRC($str) {
+    preg_match('@src="([^"]+)"@', $str, $array);
     $src = array_pop($array);
     return $src;
-}
-
-
-// Captura de items
-$items = $xml->channel->item;
-
-
-echo "<h1>Últimos Artículos</h1>";
-
-
-
-$count = 0;
-
-foreach ($items as $item) {  
-    
-    if ($count === 1)
-        break;
-    
-    $description = $item->description;
-    
-    echo extraerSRC($description);
-    
-    $explode = explode("<p>", $description);
-    echo '<br><br>';
-    echo $explode[1];
-    
-    //echo $description;
-    
-    /*
-    $data = "";
-    $data .= "
-    <script>
-        const image = document
-    </script>
-    ";
-    */
-    
-    //echo $item->title;
-    
-    //echo $item->link;
-    //echo $item->description;
-    //echo '<br>';
-    $count++;
 }
--- index.php
+++ index.php
@@ -9,6 +9,45 @@
     </head>
     
     <body>
-        
+       <?php 
+            // Captura de items
+$items = $xml->channel->item;
+
+
+
+$count = 0;
+
+foreach ($items as $item) {  
+    
+    if ($count === 1)
+        break;
+    
+    $description = $item->description;
+    
+    echo extraerSRC($description);
+    
+    $explode = explode("<p>", $description);
+    echo '<br><br>';
+    echo $explode[1];
+    
+    //echo $description;
+    
+    /*
+    $data = "";
+    $data .= "
+    <script>
+        const image = document
+    </script>
+    ";
+    */
+    
+    //echo $item->title;
+    
+    //echo $item->link;
+    //echo $item->description;
+    //echo '<br>';
+    $count++;
+}
+        ?>        
     </body>
 </html>
