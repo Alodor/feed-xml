ceec5e0f9863b33a5f73b5a35756b373fb7d2719
Alodor <jose-a-mendez@hotmail.com>
2018-09-11 21:31:37 -0400

    Inicialización del repositorio

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include 'feed.php' ?>
+
+<!Doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+        <title>Feed Blog Post desde XML</title>
+    </head>
+    
+    <body>
+        
+    </body>
+</html>
